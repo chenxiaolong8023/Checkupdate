6c65823cf2c279663fdac4be414a22076aabfa2e
Mifen <2952277346@qq.com>
2019-05-12 02:07:36 +0800

    settings

---
+++ settings.py
@@ -0,0 +1,30 @@
+# 支持平台 漫画台, 土豪漫画, 腾讯漫画, 对应 Manhuatai Tohomh123 TencentComic
+ComecDict = {
+    'Manhuatai-武动乾坤': 'https://www.manhuatai.com/wudongqiankun',
+    'Manhuatai-斗破苍穹': 'https://www.manhuatai.com/doupocangqiong',
+    'Tohomh123-妖神记': 'https://www.tohomh123.com/yaoshenji/',
+    'Manhuatai-大主宰': 'https://www.manhuatai.com/dazhuzai',
+    'TencentComic-海贼王': 'https://ac.qq.com/Comic/comicInfo/id/505430',
+    'Tohomh123-全职法师': 'https://www.tohomh123.com/quanzhifashi/',
+    'Tohomh123-永恒至尊': 'https://www.tohomh123.com/yonghengzhizun/',
+    'TencentVedio-万界神主': 'https://v.qq.com/x/cover/y0jueuihog64xhb/j0030ajsgq9.html',
+    'TencentVedio-斗罗大陆': 'https://v.qq.com/x/cover/m441e3rjq9kwpsc/r0030jqn37g.html',
+    'TencentVedio-狐妖小红娘': 'https://v.qq.com/x/cover/0sdnyl7h86atoyt.html',
+    'TencentVedio-万界仙踪 第2季' : 'https://v.qq.com/x/cover/7s65u4bg66so7e6/y0030u40wuu.html',
+    'TencentVedio-天行九歌' : 'https://v.qq.com/x/cover/rm3tmmat4li8uul/w0030phkr6h.html',
+}
+
+# 参考 https://www.cnblogs.com/Mifen2952277346/p/10849966.html
+
+# 发送人
+from_name = 'Amd794'
+# 发送人邮箱
+from_addr = None
+# 发送人密码
+password = None
+# 收件人
+to_name = 'Your'
+# 收件人邮箱
+to_addr = None
+# 邮箱服务
+smtp_server = 'smtp.qq.com'
